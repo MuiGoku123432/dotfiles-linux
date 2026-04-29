fa754531434492b6e8b776f374a02597adc14916
cfanch06 <connortheshark705@gmail.com>
2026-04-28 19:03:19 -0500

    stuff

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ info()  { printf '\033[1;34m==> %s\033[0m\n' "$*"; }
 
 # Stow all packages
 info "Stowing packages..."
-for pkg in zsh nvim git ssh ghostty starship claude hypr waybar wofi mako; do
+for pkg in zsh nvim git ssh ghostty starship claude hypr waybar wofi mako fastfetch; do
   [[ -d "$pkg" ]] || continue
   stow --no-folding -v -t "$HOME" "$pkg" && info "$pkg stowed" || info "SKIP $pkg (conflict)"
 done
